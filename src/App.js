6982f0f844e7d9bd8dfbb322681a760906688fb5
Patrick Tavares <patricktavares3pc@gmail.com>
2022-02-25 11:36:27 -0300

    master: Passando a props para o componente botão

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import {ButtonComponent} from './Components/Button/Button'
+import { ButtonComponent } from './Components/Button/Button'
+import { useState } from 'react'
 
-const App = () =>{
-  return(
-    <div>
-      <ButtonComponent/>
-    </div>
-  )
+
+const App = () => {
+	// Usando states (opcional) que fiz para poder tocar dinâmicamente o estado
+	const [properties, setProperties] = useState('primary')
+
+	// Função para que ao clicar troque o Tema 
+	const handleChangeTheme = () => {
+	// Passando uma condição para que se um estado tiver atual, ele irá ser trocado
+		if(properties === 'primary'){
+			setProperties('secondary')
+		}else{
+			setProperties('primary')
+		}
+		
+	}
+
+	// Função que habilita o disable
+	const handleChangeDisable = () => {
+		setProperties('disable')
+	}
+
+	return (
+		<div>
+			<button onClick={handleChangeTheme}>Trocar de cor </button><br />
+			<button onClick={handleChangeDisable}>Desabilitar </button><br />
+			<ButtonComponent
+				theme={properties}
+				text='text' />
+		</div>
+	)
 }
 
 export default App
