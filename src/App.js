334aad4d4e7f28580ac217f134774f88661cc758
Patrick Tavares <patricktavares3pc@gmail.com>
2022-02-25 16:06:01 -0300

    master: importando na estulização

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import { ButtonComponent } from './Components/Button/Button'
 import { useState } from 'react'
+import '../src/style.css'
 
 
 const App = () => {
-	// Usando states (opcional) que fiz para poder tocar dinâmicamente o estado
+	// Usando states (opcional) que fiz para poder trocar dinâmicamente o estado
 	const [properties, setProperties] = useState('primary')
 
 	// Função para que ao clicar troque o Tema 
 	const handleChangeTheme = () => {
-	// Passando uma condição para que se um estado tiver atual, ele irá ser trocado
-		if(properties === 'primary'){
+		// Passando uma condição para que se um estado tiver atual, ele irá ser trocado
+		if (properties === 'primary') {
 			setProperties('secondary')
-		}else{
+		} else {
 			setProperties('primary')
 		}
-		
 	}
-
 	// Função que habilita o disable
 	const handleChangeDisable = () => {
 		setProperties('disable')
 	}
 
 	return (
-		<div>
-			<button onClick={handleChangeTheme}>Trocar de cor </button><br />
-			<button onClick={handleChangeDisable}>Desabilitar </button><br />
-			<ButtonComponent
-				theme={properties}
-				text='text' />
+		<div className='container'>
+			<div className='content'>
+				<button onClick={handleChangeTheme}>Change Color </button><br />
+				<button onClick={handleChangeDisable}>Disable </button><br />
+
+				<ButtonComponent
+					theme={properties}
+					text='Text' /> <br />
+
+				<button >none </button><br />
+				<button >arrow-right </button><br />
+				<button >check</button><br />
+				<button >refresh </button><br />
+			</div>
 		</div>
 	)
 }
