88464943f89f985a9221025305c7c5d5960d675c
Patrick Tavares <patricktavares3pc@gmail.com>
2022-02-25 11:37:16 -0300

    master: recebendo a props de forma dinâmica

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,19 +1,13 @@
 import { Button } from "./style"
-export const ButtonComponent = ()=>{    
-    return(
+export const ButtonComponent = (props) => {
+    return (
         <>
-        {/* No primeiro button o theme esta sendo passado "primary", 
+            {/* No primeiro button o theme esta sendo passado "primary", 
         entretanto, o primary é a cor padrão. Se tirar, vai continuar verde 
         */}
-            <Button 
-            theme='primary'
-            >Text</Button>
-            <Button 
-            theme='secondary'
-            >Text</Button>
-            <Button 
-            disabled
-            >Text</Button>
+            <Button
+                theme={props.theme}
+            >{props.text}</Button>
         </>
     )
 }
