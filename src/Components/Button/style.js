e1a87d2fa6648906de367cf0af9b6ffab9f5a23a
Patrick Tavares <patricktavares3pc@gmail.com>
2022-02-25 11:38:26 -0300

    master: incluindo no objeto theme a propriedade disable

--- src/Components/Button/style.js
+++ src/Components/Button/style.js
@@ -11,17 +11,19 @@ const theme = {
         default: '#F7C346',
         hover: '#FFDF77',
         active: '#DCAC0D',
+    },
+    disable: {
+        cursor: 'default',
+        default: '#B7D0B6'
     }
-}
-const icons ={
-    
 }
 
 // As props estão sendo passadas por um Objeto com propriedades tornando mais dinâmico
-export const Button = styled.button`
+export const Button = styled.div`
     box-sizing: border-box;
     background-color:${props => theme[props.theme].default};
     min-width: 311px;
+    width: 311px;
     height: 56px;
     max-width: 100%;
     color: #FFF;
@@ -29,20 +31,30 @@ export const Button = styled.button`
     border: none;
     margin: 5px;
     cursor: pointer;
+    display: flex;
+    align-items: center;
     
-    &:hover{
-        background-color: ${props => theme[props.theme].hover};
-        transition: ease background-color 250ms;
+    span{
+        
+        float: right;
+        margin-right:18px ;
     }
-    &:active{
-        background-color: ${props => theme[props.theme].active};
-    }
-    &:disabled{
-        cursor: default;
-        background-color: #B7D0B6;
-    }
-`
 
+        &:hover{
+            background-color: ${props => theme[props.theme].hover};
+            transition: ease background-color 250ms;
+        }
+        &:active{
+            background-color: ${props => theme[props.theme].active};
+        }
+        &:disabled{
+            cursor: ${props => theme[props.theme].cursor};
+            background-color:${props => theme[props.theme].default} ;
+        }
+        
+
+        
+`
 // Criando a cor padrão do componente 
 Button.defaultProps = {
     theme: 'primary'
